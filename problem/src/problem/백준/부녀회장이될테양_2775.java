f2e86f61b1eec4845105ab51ae902c16d77bfb63
JingGa2022 <ad1230023@gmail.com>
2022-03-14 01:32:31 +0900

    22-03-14

--- problem/src/problem/백준/부녀회장이될테양_2775.java
+++ problem/src/problem/백준/부녀회장이될테양_2775.java
@@ -10,14 +10,21 @@ public class 부녀회장이될테양_2775 {
 			int k = sc.nextInt();
 			int n = sc.nextInt();
 			int sum = 0;
-			int[] home = new int[k*(n+1)];
-			for(int i = 1; i<=n; i++) sum += i;
-			home[0] = sum;
-			for(int i = 1; i<k*n;i++) {home[i] += home[i-1];
-			System.out.println(home[i]);
+			int[][] home = new int[k+1][n+1];
+			//0층 집어넣기
+			for(int i = 1; i<=n; i++) {
+				home[0][i] += i;
 			}
 			
-			System.out.println(home[k*n-1]);
+			for(int i=1; i<=k; i++) {
+				for(int j=1; j<=n; j++) {	
+						for(int b=1; b<=j; b++) {
+							home[i][j] += home[i-1][b];
+					}
+				}
+			}
+			
+			System.out.println(home[k][n]);
 		}
 	}
 }
