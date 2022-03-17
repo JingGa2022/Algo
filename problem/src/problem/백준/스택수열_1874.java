47e8f7eb9471121489fa398ea4f3258761a0b6c0
JingGa2022 <ad1230023@gmail.com>
2022-03-18 02:08:55 +0900

    22-03-18

--- problem/src/problem/백준/스택수열_1874.java
+++ problem/src/problem/백준/스택수열_1874.java
@@ -1,24 +1,48 @@
 package problem.백준;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class 스택수열_1874 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		boolean flag = true;
-		int room = 1;//벌집의 개수
-		int ans = 1;//방의 수
-		while(flag) {
-			if(N==1) break;
-			if(N <= room + 6*ans && N > room) {
-			ans++;
-			flag = false;
-			}else {
-				room += 6*ans;
-				ans++;
+		int[] num = new int[N];
+		Stack<Integer> stack = new Stack<>();
+		List<Character> list = new ArrayList<>();
+		for(int i = 0; i < N; i++) {
+			num[i] = sc.nextInt();
+		}
+		//수열 찾기
+		int idx =0;
+		int seq = 2;
+		stack.push(1);
+		list.add('+');
+		for(int i = 1; i < N*2; i++) {
+			if(stack.size() == 0) {
+				stack.push(seq);
+				seq++;
+				list.add('+');
+			}
+			if(stack.peek() == num[idx]) {
+				stack.pop();
+				idx++;
+				list.add('-');
+			}else if(stack.peek() != num[idx]) {
+				stack.push(seq);
+				seq++;
+				list.add('+');
 			}
 		}
-		System.out.println(ans);
-	}
+		for(int i = 0; i < list.size(); i++) {
+			if(stack.size()==0)
+			System.out.println(list.get(i));
+			else {
+				System.out.println("NO");
+				break;
+			}
+		}
+	}//main
 }
--- problem/src/problem/백준/좌표정렬하기_11650.java
+++ problem/src/problem/백준/좌표정렬하기_11650.java
@@ -8,7 +8,8 @@ public class 좌표정렬하기_11650 {
 		int N = sc.nextInt();
 		int[][] spot = new int[N][2];//
 		int[][] idx = new int[200001][2];//인덱스
-		int[][] spot2 = new int[N][2];//정렬후
+		int[][] spot2 = new int[N][2];//1차 정렬후
+		int[][] spot3 = new int[N][2];//2차 정렬후
 		for(int i =0; i<N; i++) {
 			spot[i][0] = sc.nextInt();
 			spot[i][1] = sc.nextInt();
@@ -26,11 +27,21 @@ public class 좌표정렬하기_11650 {
 			idx[i][0] += idx[i-1][0];
 		}
 		for(int i = N-1; i > 0; i--) {
-			if(spot[i][0] >= 0)
-				idx[spot[i][0]+100000][0] ++;
-			else
-			idx[spot[i][0]][0] ++;
+			if(spot[i][0] >= 0) {
+				spot2[idx[spot[i][0]+100000][0]][0] = spot[i][0];
+				spot2[idx[spot[i][0]+100000][0]][1] = spot[i][1];//두번째도 따라가야하니까
+			idx[spot[i][0]+100000][0] --;
+			}
+			else {
+				spot2[idx[spot[i][0]][0]][0] = spot[i][0];
+				spot2[idx[spot[i][0]][0]][1] = spot[i][1];
+				idx[spot[i][0]][0] --;
+			}
 		}
-		
+		//그리고 0번재 것이 같다면 1번쨰를 카운팅 정렬하게 하면 됨
+		for(int i = N-1; i>0; i++) {
+			System.out.println(spot2[N][i]);
+			
+		}
 	}//main
 }
