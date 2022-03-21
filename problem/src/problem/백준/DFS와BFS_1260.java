bded01edbce9303923b5b22e966905f59493aeb1
JingGa2022 <ad1230023@gmail.com>
2022-03-21 23:35:41 +0900

    22-03-21

---
+++ problem/src/problem/백준/DFS와BFS_1260.java
@@ -0,0 +1,12 @@
+package problem.백준;
+
+import java.util.Scanner;
+
+public class DFS와BFS_1260 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();//정점의 개수
+		int M = sc.nextInt();//간선의 개수
+		int v = sc.nextInt();//탐색을 시작할 정점의 번호
+	}
+}
