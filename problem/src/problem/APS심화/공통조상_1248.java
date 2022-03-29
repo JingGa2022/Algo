c33d56e25a58fe1fb3b66c9a856bf8374d123f8a
JingGa2022 <ad1230023@gmail.com>
2022-03-30 01:12:57 +0900

    22-03-30

---
+++ problem/src/problem/APS심화/공통조상_1248.java
@@ -0,0 +1,21 @@
+package problem.APS심화;
+
+import java.util.Scanner;
+
+public class 공통조상_1248 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		int T = sc.nextInt();
+		for(int tc = 1; tc <= T; tc++) {
+			int V = sc.nextInt();//정점의 수
+			int E = sc.nextInt();//간선의 수
+			int vNum1 = sc.nextInt();//정점번호1
+			int vNum2 = sc.nextInt();//정점번호2
+			int[] num = new int[V];
+			for(int i =0; i<V; i++) {
+				num[i] = ;
+			}
+		}
+	}
+}
