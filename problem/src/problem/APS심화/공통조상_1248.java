c663636b891ff2b8ad3f2aee628c0c3cd47cac58
JingGa2022 <ad1230023@gmail.com>
2022-03-30 01:43:11 +0900

    22-03-30

--- problem/src/problem/APS심화/공통조상_1248.java
+++ problem/src/problem/APS심화/공통조상_1248.java
@@ -12,9 +12,10 @@ public class 공통조상_1248 {
 			int E = sc.nextInt();//간선의 수
 			int vNum1 = sc.nextInt();//정점번호1
 			int vNum2 = sc.nextInt();//정점번호2
-			int[] num = new int[V];
-			for(int i =0; i<V; i++) {
-				num[i] = ;
+			int[][] num = new int[V+1][3];
+			for(int i =1; i<=V; i++) {
+				for(int j =1; j <=2 ; i++)
+					num[i][j] = sc.nextInt();//숫자를  행으로 삼아 1열은 왼자 2열은 오자
 			}
 		}
 	}
