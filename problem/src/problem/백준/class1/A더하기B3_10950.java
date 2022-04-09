c28f07126b2a5f253ca26602e550c6cebf41811b
JingGa2022 <ad1230023@gmail.com>
2022-04-10 00:41:34 +0900

    22-04-10

---
+++ problem/src/problem/백준/class1/A더하기B3_10950.java
@@ -0,0 +1,14 @@
+package problem.백준.class1;
+
+import java.util.Scanner;
+
+public class A더하기B3_10950 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int tc = 0; tc<T; tc++)
+			System.out.println(sc.nextInt() + sc.nextInt());
+	}
+}
