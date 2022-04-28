58f07e2c7f8f6092d89fc73c26d6ed982b8d1195
JingGa2022 <ad1230023@gmail.com>
2022-04-28 20:22:00 +0900

    22-04-28

--- problem/src/problem/백준/class2/수정렬하기2_2751.java
+++ problem/src/problem/백준/class2/수정렬하기2_2751.java
@@ -4,12 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class 수정렬하기3_10989 {
-	public static void main(String[] args) throws IOException {
+public class 수정렬하기2_2751 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
-		int[] idx = new int[10001]; // N에 가능한 수의 범위
+		int[] idx = new int[1000001]; // N에 가능한 수의 범위
 
 		for (int tc = 0; tc < N; tc++) {
 			idx[Integer.parseInt(br.readLine())]++;
@@ -18,7 +18,7 @@ public class 수정렬하기3_10989 {
 		br.close();
 
 		// 0은 없으므로 1부터 시작한다
-		for (int i = 1; i < 10001; i++) {
+		for (int i = 1; i < 1000001; i++) {
 			while (idx[i] > 0) {
 				sb.append(i).append("\n");
 				idx[i]--;
@@ -27,5 +27,4 @@ public class 수정렬하기3_10989 {
 		
 		System.out.println(sb);
 	}// main
-
 }
