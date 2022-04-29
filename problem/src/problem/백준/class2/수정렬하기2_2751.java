e97cfad4b29611c5447db9d1f6c7a3e508afa8e7
JingGa2022 <ad1230023@gmail.com>
2022-04-29 16:16:28 +0900

    22-04-29

--- problem/src/problem/백준/class2/수정렬하기2_2751.java
+++ problem/src/problem/백준/class2/수정렬하기2_2751.java
@@ -1,30 +1,28 @@
 package problem.백준.class2;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
 public class 수정렬하기2_2751 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Scanner sc = new Scanner(System.in);
 		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
-		int[] idx = new int[1000001]; // N에 가능한 수의 범위
-
-		for (int tc = 0; tc < N; tc++) {
-			idx[Integer.parseInt(br.readLine())]++;
-		}
-
-		br.close();
-
-		// 0은 없으므로 1부터 시작한다
-		for (int i = 1; i < 1000001; i++) {
-			while (idx[i] > 0) {
-				sb.append(i).append("\n");
-				idx[i]--;
-			}
+		//힙 사용
+		
+		int N = sc.nextInt();
+		PriorityQueue<Integer> heap = new PriorityQueue<>();
+		int[] arr = new int[N];
+		
+		//배열의 힙으로 만든다.
+		for(int i = 0; i<N; i++) {
+			arr[i] = sc.nextInt();
+			heap.add(arr[i]);
 		}
 		
+		for(int i =0; i<N; i++) sb.append(heap.poll()).append("\n");
+		
 		System.out.println(sb);
+		
 	}// main
 }
