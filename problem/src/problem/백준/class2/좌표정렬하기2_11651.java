70873feaf096fcbbf97be49a792a8acf625bef40
JingGa2022 <98095173+JingGa2022@users.noreply.github.com>
2022-05-14 00:00:55 +0900

    22-05-14

---
+++ problem/src/problem/백준/class2/좌표정렬하기2_11651.java
@@ -0,0 +1,37 @@
+package problem.백준.class2;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class 좌표정렬하기2_11651 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		int N = sc.nextInt();
+		
+		int[][] arr = new int[N][2];
+		
+		for(int i = 0; i<N; i++) {
+			arr[i][0] = sc.nextInt();
+			arr[i][1] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr, new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if(o1[1] == o2[1])
+					return o1[0] - o2[0];
+				else
+					return o1[1] - o2[1];
+			}
+		});
+		
+		for(int i =0; i<N; i++) {
+			sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
