b4297e46368781863dc875901c22b4bcc9f9c910
JingGa2022 <ad1230023@gmail.com>
2022-05-17 00:02:56 +0900

    22-05-17

--- problem/src/problem/백준/class2/체스판다시칠하기_1018.java
+++ problem/src/problem/백준/class2/체스판다시칠하기_1018.java
@@ -1,7 +1,35 @@
 package problem.백준.class2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class 체스판다시칠하기_1018 {
-	public static void main(String[] args) {
+	static String[][] box;
+	static int ans = 0;
+	static int N; // 행
+	static int M; // 열
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str);
+		//개행제거
+		br.read();
 		
+		N = Integer.parseInt(st.nextToken()); // 행
+		M = Integer.parseInt(st.nextToken()); // 열
+		box = new String[N][M];
+		
+		
+		for(int i = 0; i<N; i++) {
+				box[i] = br.readLine().split("");
+		}
+		
+		for(int i =0; i<= N-7; i++) {
+			
+		}
 	}
 }
