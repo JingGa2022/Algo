2832b3b921f327820c0cb9bde480a0c4e2b458ff
JingGa2022 <ad1230023@gmail.com>
2022-05-23 02:35:46 +0900

    22-05-23

---
+++ problem/src/problem/백준/물음표물음표느낌표_10926.java
@@ -0,0 +1,13 @@
+package problem.백준;
+
+import java.util.Scanner;
+
+public class 물음표물음표느낌표_10926 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String id = sc.next();
+		
+		System.out.printf("%s??!",id);
+	}
+}
