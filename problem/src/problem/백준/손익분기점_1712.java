2ab95daaaa2afe6d16b3256b00996266479c34b7
JingGa2022 <ad1230023@gmail.com>
2022-05-22 16:30:33 +0900

    22-05-22

---
+++ problem/src/problem/백준/손익분기점_1712.java
@@ -0,0 +1,24 @@
+package problem.백준;
+
+import java.util.Scanner;
+
+public class 손익분기점_1712 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int cnt = 1;
+		
+		int sum = a+b;
+		
+		if(b>=c) {
+			System.out.println(-1);
+		}else {
+			System.out.println((a/(c-b))+1);
+		}
+		
+	}
+}
