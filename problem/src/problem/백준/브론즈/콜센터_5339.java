7e91653b44aeae4a895972bc782ba6c5aca26f06
안웅진 <ad1230023@gmail.com>
2022-06-15 00:04:58 +0900

    22-06-15

---
+++ problem/src/problem/백준/브론즈/콜센터_5339.java
@@ -0,0 +1,16 @@
+package problem.백준.브론즈;
+
+public class 콜센터_5339 {
+	public static void main(String[] args) {
+		System.out.println("     /~\\");
+		System.out.println("    ( oo|");
+		System.out.println("    _\\=/_");
+		System.out.println("   /  _  \\");
+		System.out.println("  //|/.\\|\\\\");
+		System.out.println(" ||  \\ /  ||");
+		System.out.println("============");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		System.out.println("|          |");
+	}
+}
