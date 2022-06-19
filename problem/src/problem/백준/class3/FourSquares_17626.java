460d099d5eca1746591e135ec84c8e355bc8978e
안웅진 <ad1230023@gmail.com>
2022-06-19 17:51:04 +0900

    22-06-19

---
+++ problem/src/problem/백준/class3/FourSquares_17626.java
@@ -0,0 +1,26 @@
+package problem.백준.class3;
+
+import java.util.Scanner;
+
+public class FourSquares_17626 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int[] dp = new int[n+1];
+		
+		dp[0] = 0;
+		dp[1] = 1;
+		
+		for(int i = 2; i<= n; i++) {
+			int min = Integer.MAX_VALUE;
+			for(int j = 1; j*j<= i ; j++) {
+				min = Math.min(min, dp[i-j*j]);
+			}
+			dp[i] = min + 1;
+		}
+		System.out.println(dp[n]);
+		
+	}
+}
