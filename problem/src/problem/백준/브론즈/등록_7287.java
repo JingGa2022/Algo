2cf14f06f2dc3cbcd0892e0b63a937ec57ceeb09
안웅진 <ad1230023@gmail.com>
2022-06-27 00:00:56 +0900

    22-06-27

---
+++ problem/src/problem/백준/브론즈/등록_7287.java
@@ -0,0 +1,8 @@
+package problem.백준.브론즈;
+
+public class 등록_7287 {
+	public static void main(String[] args) {
+		System.out.println(127);
+		System.out.println("ad12300");
+	}
+}
--- problem/src/problem/백준/실버/소수_1312.java
+++ problem/src/problem/백준/실버/소수_1312.java
@@ -1,5 +1,24 @@
 package problem.백준.실버;
 
-public class 소수_1312 {
+import java.util.Scanner;
 
+public class 소수_1312 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int n = sc.nextInt();
+		
+		int ans = a%b;
+		
+		for(int i = 0; i<n-1; i++) {
+			ans *= 10;
+			ans %= b;
+		}
+		
+		ans *= 10;
+		System.out.println(ans/b);
+	}
 }
