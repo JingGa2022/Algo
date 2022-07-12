874ec4f70fa00ba22cdcb221ba766faf0bd283c2
안웅진 <ad1230023@gmail.com>
2022-07-13 00:50:26 +0900

    22-07-13
    DP 연습

--- problem/src/problem/백준/class3/피보나치함수_1003.java
+++ problem/src/problem/백준/class3/피보나치함수_1003.java
@@ -3,44 +3,38 @@ package problem.백준.class3;
 import java.util.Scanner;
 
 public class 피보나치함수_1003 {
-	static int[][] list;
+	static int[][] dp;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		int tc = sc.nextInt();
-		int max = 0;
-		int[] nums = new int[tc];
-		
-		for(int i = 0; i<tc; i++) {
-			nums[i] = sc.nextInt();
-			max = Math.max(max, nums[i]);
+
+		int t = sc.nextInt();
+
+		for (int i = 0; i < t; i++) {
+			int tmp = sc.nextInt();
+			dp = new int[tmp + 2][2];
+			
+			dp[0][0] = dp[1][1] = 1;
+			dp[0][1] = dp[1][0] = 0;
+			fibo(tmp);
+			System.out.printf("%d %d\n", dp[tmp][0], dp[tmp][1]);
 		}
-		int[] arr = new int[2];
-		
-		list = new int[max + 1][2];
-		list[0][0]++;
-		list[1][1]++;
-		
-		fibo(max, arr);
-		
-		for (int t = 0; t < tc; t++) {
-			int idx = nums[t];
 
-			arr[0] = list[idx][0];
-			arr[1] = list[idx][1];
-
-			System.out.printf("%d %d\n", arr[0], arr[1]);
-		} // for
 	}// main
 
-	public static void fibo(int num, int[] arr) {
-		if(num == 0 && num == 1) return;
-		else {
-			for(int i = 2; i<=num; i++) {
-				list[i][0] = list[i-1][0] + list[i-2][0];
-				list[i][1] = list[i-1][1] + list[i-2][1];
+	private static void fibo(int tmp) {
+		// TODO Auto-generated method stub
+		for (int i = 2; i <= tmp; i++) {
+			if (dp[i][0] == 0) {
+				dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
 			}
+			if (dp[i][1] == 0) {
+				dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+			} else {
+				continue;
+			}
+
 		}
 	}
+
 }
