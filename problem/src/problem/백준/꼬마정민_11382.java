f451d4ecba737c6044453255a051d480c875dde5
안웅진 <98095173+JingGa2022@users.noreply.github.com>
2022-08-06 14:33:01 +0900

    22-08-06
    큰수 연습

---
+++ problem/src/problem/백준/꼬마정민_11382.java
@@ -0,0 +1,17 @@
+package problem.백준;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class 꼬마정민_11382 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		BigInteger A = sc.nextBigInteger();
+		BigInteger B = sc.nextBigInteger();
+		BigInteger C = sc.nextBigInteger();
+		
+		System.out.println(A.add(B).add(C));
+	}
+	
+}
