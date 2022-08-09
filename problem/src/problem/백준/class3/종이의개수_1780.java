cf0a2234e85370f4ef2bb54ffe3284666433c175
안웅진 <98095173+JingGa2022@users.noreply.github.com>
2022-08-10 00:13:33 +0900

    22-08-10
    분할정복 연습
    재귀 연습

--- problem/src/problem/백준/class3/종이의개수_1780.java
+++ problem/src/problem/백준/class3/종이의개수_1780.java
@@ -11,7 +11,7 @@ public class 종이의개수_1780 {
 		n = sc.nextInt();
 		arr = new int[n][n];
 		ans_1= 0;//-1
-		ans0=0;//0
+		ans0= 0;//0
 		ans1 = 0;//1
 		
 		for(int i =0; i<n; i++) {
@@ -26,41 +26,72 @@ public class 종이의개수_1780 {
 		System.out.println(ans1);
 	}
 	private static void square(int x, int y, int len) {
-		int newLen = len/3;
-		
-		if(newLen == 0) return;
-		
-		for(int i = 0; i<9 ;i++) {
-			int newX = x + (newLen * i/3);
-			int newY = y + (newLen * i%3);
-			int check = paperCheck(newX, newY, newLen);
-			if(check == 2) {
-				square(newX, newY, newLen);
-			}else {
-				switch (check) {
-				case 1:
-					ans1++;
-					break;
-				case 0:
-					ans0++;
-					break;
-				case -1:
-					ans_1++;
-					break;
+		if(check(x,y, len)) {
+			switch (arr[x][y]) {
+			case 1:
+				ans1++;
+				break;
+			case 0:
+				ans0++;
+				break;
+			case -1:
+				ans_1++;
+				break;
+			}
+		}else {
+			int newLen = len/3;
+			for(int i = 0; i<3; i++) {
+				for(int j = 0; j<3; j++) {
+					square(x+ newLen*i, y + newLen*j, newLen);
 				}
 			}
 		}
-		
-		paperCheck(x, y, newLen);
 	}
-	private static int paperCheck(int x, int y, int len) {
+	private static boolean check(int x, int y, int len) {
 		int tmp = arr[x][y];
-		
-		for(int i = 0 ;i<len; i++) {
-			for(int j = 0; j<len; j++) {
-				if(tmp != arr[i][j]) return 2;
+		for(int i = x; i<x+len; i++) {
+			for(int j =y; j<y+len; j++) {
+				if(tmp != arr[i][j]) return false;
 			}
 		}
-		return tmp;
+		return true;
 	}
+	
+	
+//	private static void square(int x, int y, int len) {
+//		int newLen = len/3;
+//		
+//		for(int i = 0; i<9 ;i++) {
+//			int newX = x + (newLen * i/3);
+//			int newY = y + (newLen * i%3);
+//			//System.out.printf("횟수: %d %d %d\n", i, newX, newY);
+//			int check = paperCheck(newX, newY, newLen);
+//			if(check == 2) {
+//				square(newX, newY, newLen);
+//			}else {
+//				switch (check) {
+//				case 1:
+//					ans1++;
+//					break;
+//				case 0:
+//					ans0++;
+//					break;
+//				case -1:
+//					ans_1++;
+//					break;
+//				}
+//			}
+//		}
+//		
+//	}
+//	private static int paperCheck(int x, int y, int len) {
+//		int tmp = arr[x][y];
+//		
+//		for(int i = x ;i<x+len; i++) {
+//			for(int j = y; j<y+len; j++) {
+//				if(tmp != arr[i][j]) return 2;
+//			}
+//		}
+//		return tmp;
+//	}
 }
