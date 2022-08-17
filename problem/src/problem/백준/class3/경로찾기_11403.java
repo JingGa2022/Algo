bc172490a308c2fa868f3e356c31426e60940e1c
안웅진 <ad1230023@gmail.com>
2022-08-18 00:16:55 +0900

    22-08-18
    dp 조합 연습

---
+++ problem/src/problem/백준/class3/경로찾기_11403.java
@@ -0,0 +1,11 @@
+package problem.백준.class3;
+
+import java.util.Scanner;
+
+public class 경로찾기_11403 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+	}
+}
