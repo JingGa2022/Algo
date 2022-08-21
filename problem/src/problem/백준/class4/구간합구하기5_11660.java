290776c2dda04d45aee10d7685d9294b1947da1b
안웅진 <98095173+JingGa2022@users.noreply.github.com>
2022-08-22 00:09:21 +0900

    22-08-22
    bufferedReader 추가

--- problem/src/problem/백준/class4/구간합구하기5_11660.java
+++ problem/src/problem/백준/class4/구간합구하기5_11660.java
@@ -1,39 +1,44 @@
 package problem.백준.class4;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class 구간합구하기5_11660 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		
-		int[][] arr = new int[n+1][n+1];
-		int[][] sum = new int[n+1][n+1];
-		
-		
-		for(int i = 1; i<=n; i++) {
-			for(int j = 1 ; j<=n; j++) {
-				arr[i][j] = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		int[][] arr = new int[n + 1][n + 1];
+		int[][] sum = new int[n + 1][n + 1];
+
+		for (int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= n; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
 				sum[i][j] = arr[i][j];
 			}
 		}
-		
-		//누적합을 구하고 필요없는부분 빼기
-		for(int i = 1; i<=n; i++) {
-			for(int j = 1 ; j<=n; j++) sum[i][j] = sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] + arr[i][j];
+
+		// 누적합을 구하고 필요없는부분 빼기
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++)
+				sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + arr[i][j];
 		}
-		
-		
-		for(int tc = 0; tc<m; tc++) {
+
+		for (int tc = 0; tc < m; tc++) {
+			st = new StringTokenizer(br.readLine());
 			int ans = 0;
-			int x1 = sc.nextInt();
-			int y1 = sc.nextInt() ;
-			int x2 = sc.nextInt();
-			int y2 = sc.nextInt();
-					
-				ans = sum[x2][y2] -sum[x1-1][y2] - sum[x2][y1-1] + sum[x1-1][y1 - 1];
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+
+			ans = sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1];
 
 			System.out.println(ans);
 		}
