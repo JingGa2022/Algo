0edb500927f66c61227f268a0f0d7a1ee5f44648
안웅진 <ad1230023@gmail.com>
2022-09-03 01:05:01 +0900

    22-09-03
    구현 연습

--- problem/src/problem/백준/class3/아기상어_16236.java
+++ problem/src/problem/백준/class3/아기상어_16236.java
@@ -1,6 +1,8 @@
 package problem.백준.class3;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -9,54 +11,58 @@ public class 아기상어_16236 {
 	static class Spot {
 		int x;
 		int y;
+		int time;
 
-		public Spot(int x, int y) {
+		public Spot(int x, int y, int time) {
 			this.x = x;
 			this.y = y;
+			this.time  = time;
 		}
 	}
 
 	static int N, size, ans;
-	static ArrayList fish = new ArrayList<>();
+	static ArrayList<Spot> fish = new ArrayList<>();
 	static Spot baby;
+	static int eaten = 0;
 	static int[] dr = { 0, -1, 0, 1 };
 	static int[] dc = { -1, 0, 1, 0 };
 	static int[][] arr;
+	static boolean[][] visit ;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
 		N = sc.nextInt();
-		size = 1;
+		size = 2;
 		ans = 0;
 		arr = new int[N][N];
-		Spot start = new Spot(0, 0);
+		visit = new boolean[N][N];
 
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				arr[i][j] = sc.nextInt();
 
 				if (arr[i][j] == 9) {
-					start = new Spot(i, j);
-					baby = new Spot(i, j);
+					baby = new Spot(i, j, 0);
+					arr[i][j] = 0;
 				}
 			}
 		}
 
-		bfs(start.x, start.y);
+		bfs(baby.x, baby.y);
 
 		System.out.println(ans);
 	}
 
 	private static void bfs(int x, int y) {
 		Queue<Spot> q = new LinkedList<>();
-		boolean[][] visit = new boolean[N][N];
 
-		q.add(new Spot(x, y));
+		q.add(baby);
 
 		visit[x][y] = true;
-
-		outer: while (!q.isEmpty()) {
+		while(true) {
+			
+		while (!q.isEmpty()) {
 			Spot spot = q.poll();
 
 			for (int i = 0; i < 4; i++) {
@@ -66,19 +72,71 @@ public class 아기상어_16236 {
 				if (newX < 0 || newY < 0 || newX >= N || newY >= N || visit[newX][newY])
 					continue;
 
-				if (arr[newX][newY] <= size) {
-					q.add(new Spot(newX, newY));
-					if (arr[newX][newY] != 0) {
-						fish.add(arr[newX][newY]);
-						ans += Math.abs(newX - x) + Math.abs(newY - y);
-						arr[x][y] = 0;
-						arr[newX][newY] = 9;
-
+				if (arr[newX][newY] < size && arr[newX][newY] != 0) {
+					q.add(new Spot(newX, newY, spot.time + 1));
+					visit[newX][newY] = true;
+					fish.add(new Spot(newX, newY, spot.time + 1));
+				}
+					if (arr[newX][newY] == size || arr[newX][newY] == 0) {
+						q.add(new Spot(newX, newY, spot.time + 1));
+						visit[newX][newY] = true;
 					}
 				}
 			}
+			//식사후
+			if(!fish.isEmpty()) {
+				eat();
+				
+				q.clear();
+				visit = new boolean[N][N];
+				
+				q.add(baby);
+				visit[baby.x][baby.y] = true;
+			}else
+				return;
 		}
+	}
 
+	private static void eat() {
+		// TODO Auto-generated method stub
+		Collections.sort(fish, new Comparator<Spot>() {
+
+			@Override
+			public int compare(Spot o1, Spot o2) {
+				if(o1.time == o2.time) {
+					if(o1.x == o2.x) {
+						if(o1.y > o2.y) {
+							return 1;
+						}else
+							return -1;
+					}else {
+						if(o1.x > o2.x) {
+							return 1;
+						}else
+							return -1;
+					}
+				}else if(o1.time > o2.time) {
+					return 1;
+				}else
+					return -1;
+			};
+			
+		});
+		Spot spot = fish.get(0);
+		
+		baby.x = spot.x;
+		baby.y = spot.y;
+		
+		if(++eaten == size) {
+			size ++;
+			eaten = 0;
+		}
+		
+		ans += spot.time;
+		
+		arr[spot.x][spot.y] = 0;
+		
+		fish.clear();
 	}
 
 }
