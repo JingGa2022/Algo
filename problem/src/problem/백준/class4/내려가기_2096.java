c0b0e4cbf541edc2e3cdba00e0dcdd5f4b5e9558
안웅진 <ad1230023@gmail.com>
2022-09-22 20:35:29 +0900

    2022-09-22
    dp 연습

---
+++ problem/src/problem/백준/class4/내려가기_2096.java
@@ -0,0 +1,13 @@
+package problem.백준.class4;
+
+import java.util.Scanner;
+
+public class 내려가기_2096 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		
+	}
+}
