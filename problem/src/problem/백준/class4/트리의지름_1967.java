2d33c861f98dfb9612d9df2d1eedf344a952c38f
안웅진 <ad1230023@gmail.com>
2022-10-01 00:20:29 +0900

    22-10-01
    dfs 연습

--- problem/src/problem/백준/class4/트리의지름_1967.java
+++ problem/src/problem/백준/class4/트리의지름_1967.java
@@ -1,49 +1,63 @@
 package problem.백준.class4;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class 트리의지름_1967 {
-	static int n, maxL, maxR, ans;
-	static int[][] tree;
+	static class Node{
+		int num;
+		int weight;
+		
+		public Node(int num, int weight) {
+			this.num = num;
+			this.weight = weight;
+		}
+	}
+	static int n, max, ans;
+	static ArrayList<Node>[] tree;
+	static boolean[] visit;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		n = sc.nextInt();
-		tree = new int[n+1][n+1];
+		tree = new ArrayList[n+1];
+		visit = new boolean[n+1];
 		
-		maxL = 0;
-		maxR = 0;
 		ans = 0;
 		
-		for(int i =0; i<n ;i++) {
+		for(int i = 0 ;i <=n ;i++) {
+			tree[i] = new ArrayList<>();
+		}
+		
+		for(int i =0; i<n-1 ;i++) {
 			int from = sc.nextInt();
 			int to = sc.nextInt();
 			int path = sc.nextInt();
-			tree[from][to] = path;
+			tree[from].add(new Node(to, path));
+			tree[to].add(new Node(from, path));
 		}
-		System.out.println("여기까진 왔어요");
-		dfs(1, 0, false);
-		ans = maxL + maxR;
+		visit[1] = true;
+		dfs(1, 0);
+		
+		visit = new boolean[n+1];
+		visit[max] = true;
+		dfs(max, 0);
 		
 		System.out.println(ans);
 	}
-	// false면 왼쪽 true면 오른쪽
-	private static void dfs(int from, int sum, boolean isRight) {
+	
+	private static void dfs(int from, int sum) {
 		// TODO Auto-generated method stub
-		boolean flag = false;
-		
-		for(int i = 2; i<=n; i++) {
-			if(tree[from][i] != 0) {
-				if(from == 1) isRight = !isRight; 
-				
-				dfs(i, sum + tree[from][i], isRight);
-				flag = true;
-			}
+		if(sum > ans) {
+			ans =  sum;
+			max = from;
 		}
-		if(!flag && isRight) {
-			if(sum > maxR) maxR = sum;
-		}else if(!flag && !isRight) {
-			if(sum > maxL) maxL = sum;
+		for(Node node: tree[from]) {
+			if(!visit[node.num]) {
+				visit[node.num] = true;
+				dfs(node.num, sum + node.weight);
+			}
 		}
 	}
 }
