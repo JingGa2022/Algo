7e696ee8a5b5202b6c53632395196ee1ba3f0234
안웅진 <ad1230023@gmail.com>
2022-10-18 00:23:30 +0900

    22-10-18
    정렬 연습

---
+++ problem/src/problem/백준/수정렬하기_2759.java
@@ -0,0 +1,26 @@
+package problem.백준;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 수정렬하기_2759 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		int n = sc.nextInt();
+		
+		int[] arr = new int[n];
+		
+		for(int i = 0; i<n ;i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i = 0; i<n ;i++) {
+			sb.append(arr[i]+"\n");
+		}
+		System.out.println(sb);
+	}
+}
