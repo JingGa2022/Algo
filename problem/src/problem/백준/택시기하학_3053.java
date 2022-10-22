af78fe8404193f679565e2789b80653e15170f60
안웅진 <ad1230023@gmail.com>
2022-10-22 14:09:48 +0900

    22-10-22
    기하학 연습

---
+++ problem/src/problem/백준/택시기하학_3053.java
@@ -0,0 +1,14 @@
+package problem.백준;
+
+import java.util.Scanner;
+
+public class 택시기하학_3053 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double r = sc.nextDouble();
+		
+		System.out.println(r * r * Math.PI);
+		System.out.println(r * r * 2);
+	}
+}
