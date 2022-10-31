4244b6bdd19cf2d537cab560b4b0d4db8b2f60a7
안웅진 <ad1230023@gmail.com>
2022-11-01 00:29:27 +0900

    22-11-01

---
+++ problem/src/problem/백준/새싹_25083.java
@@ -0,0 +1,15 @@
+package problem.백준;
+
+public class 새싹_25083 {
+	public static void main(String[] args) {
+
+		String s = "         ,r'\"7\n" + 	
+				   "r`-_   ,'  ,/\n" + 		
+				   " \\. \". L_r'\n" + 		
+				   "   `~\\/\n" + 			
+				   "      |\n" + 			
+				   "      |";
+		
+		System.out.println(s);
+	}
+}
