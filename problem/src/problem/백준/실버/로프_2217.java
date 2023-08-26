91894c30ef616a7c60a5ac3d4969ce91e8e1a43d
안웅진 <ad1230023@gmail.com>
2023-08-26 16:44:34 +0900

    23-08-26
    알고 연습 다시 시작!
    Greedy Algorithm

---
+++ problem/src/problem/백준/실버/로프_2217.java
@@ -0,0 +1,11 @@
+package problem.백준.실버;
+
+import java.util.Scanner;
+
+public class 로프_2217 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+	}
+}
